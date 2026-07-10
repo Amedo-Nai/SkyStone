8de927ba652ae5eb93881fd8b68818416c4b9738
Amedo-Nai <amedonai123@gmail.com>
2026-07-10 16:56:36 +0400

    Starting the development of SKYSTONE:CELESTIAL THREAT

---
+++ src/client/java/name/ss_celestial_threat/client/SkyStoneCelestialThreatDataGenerator.java
@@ -0,0 +1,11 @@
+package name.ss_celestial_threat.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkyStoneCelestialThreatDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
